6577f94362d2ff4c35c02d098075584d5054aaf4
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-01-19 11:14:29 +0100

    RecyclerView acabado, detalles en proceso
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/CustomRecyclerViewAdapter.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/CustomRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package net.ddns.smartfridge.smartfridgev2.modelo;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -18,6 +19,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import net.ddns.smartfridge.smartfridgev2.R;
 import net.ddns.smartfridge.smartfridgev2.vista.MiNeveraActivity;
+import net.ddns.smartfridge.smartfridgev2.vista.SugerirRecetaActivity;
 
 import java.io.ByteArrayOutputStream;
 import java.sql.Blob;
@@ -27,61 +29,54 @@ import java.util.ArrayList;
  * Created by Alberto on 17/01/2018.
  */
 
-public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder>{
+public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder> {
     private Cursor cursor;
     private MiNeveraActivity activity;
-    ArrayList<Alimento> alimentos;
+    private ArrayList<Alimento> alimentos;
+    private ArrayList<Alimento> alimentosCopia;
 
-    public CustomRecyclerViewAdapter(Cursor cursor, MiNeveraActivity activity){
+    public CustomRecyclerViewAdapter(Cursor cursor, MiNeveraActivity activity) {
         this.cursor = cursor;
         this.activity = activity;
         alimentos = new ArrayList();
-        //_id", "nombre", "cantidad", "dias_caducidad", "fecha_registro", "fecha_caducidad", "imagen_alimento", "id_alimento_creado
-        //int id, String nombreAlimento, int cantidad, int dias_caducidad, String fecha_registro, String fecha_caducidad, Bitmap imagen
         cargarArray();
+        alimentosCopia = new ArrayList<>();
+        alimentosCopia.addAll(alimentos);
         Log.d("RAQUEL", "CustomRecyclerViewAdapter: " + Environment.getExternalStorageDirectory().getAbsolutePath());
     }
 
-    private void cargarArray(){
-        if(cursor.moveToFirst()){
+    private void cargarArray() {
+        if (cursor.moveToFirst()) {
             byte[] byteArrayFoto;
-            Bitmap bm;
+            Bitmap bm = null;
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
             for (int i = 0; i < cursor.getCount(); i++) {
                 byteArrayFoto = cursor.getBlob(6);
-                if(byteArrayFoto != null) {
+                if (byteArrayFoto != null) {
                     byteArrayFoto = cursor.getBlob(6);
                     bm = BitmapFactory.decodeByteArray(byteArrayFoto, 0, byteArrayFoto.length);
                     bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                    alimentos.add(new Alimento(
-                            cursor.getInt(0),
-                            cursor.getString(1),
-                            cursor.getInt(2),
-                            cursor.getInt(3),
-                            cursor.getString(4),
-                            cursor.getString(5),
-                            bm));
-                }else{
-                    alimentos.add(new Alimento(
-                            cursor.getInt(0),
-                            cursor.getString(1),
-                            cursor.getInt(2),
-                            cursor.getInt(3),
-                            cursor.getString(4),
-                            cursor.getString(5),
-                            null));
                 }
+                alimentos.add(new Alimento(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getInt(2),
+                        cursor.getInt(3),
+                        cursor.getString(4),
+                        cursor.getString(5),
+                        bm));
+
             }
         }
     }
 
-    private void llenarFila(CustomRecyclerViewAdapter.ViewHolder holder, int position){
+    private void llenarFila(CustomRecyclerViewAdapter.ViewHolder holder, int position) {
         holder.tvNombre.setText(alimentos.get(position).getNombreAlimento());
         holder.tvUnidades.setText(String.valueOf(alimentos.get(position).getCantidad()));
-        holder.tvDiasCaducidad.setText(alimentos.get(position).getDias_caducidad());
+        holder.tvDiasCaducidad.setText(String.valueOf(alimentos.get(position).getDias_caducidad()));
         holder.tvFechaCaducidad.setText(formatearFecha(alimentos.get(position).getFecha_caducidad()));
 
-        if(alimentos.get(position).getImagen() != null){
+        if (alimentos.get(position).getImagen() != null) {
             try {
                 Glide.with(this.activity.getApplicationContext())
                         .load(alimentos.get(position).getImagen())
@@ -89,43 +84,42 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecycl
                         .diskCacheStrategy(DiskCacheStrategy.NONE)
                         .skipMemoryCache(true)
                         .into(holder.ivFotoAlimento);
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-       }
-
-        /*cursor.moveToPosition(position);
-
-        holder.tvNombre.setText(cursor.getString(1));
-        holder.tvUnidades.setText(String.valueOf(cursor.getInt(2)));
-        holder.tvDiasCaducidad.setText(cursor.getString(3));
-        holder.tvFechaCaducidad.setText(formatearFecha(cursor.getString(5)));
-        byte[] byteArrayFoto = cursor.getBlob(6);
-        if(byteArrayFoto != null){
-            Bitmap bm = BitmapFactory.decodeByteArray(byteArrayFoto, 0 ,byteArrayFoto.length);
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            try {
-                Glide.with(this.activity.getApplicationContext())
-                        .load(stream.toByteArray())
-                        .asBitmap()
-                        .diskCacheStrategy(DiskCacheStrategy.NONE)
-                        .skipMemoryCache(true)
-                        .into(holder.ivFotoAlimento);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-        }*/
+        }
     }
 
-    private String formatearFecha(String fecha){
+    private String formatearFecha(String fecha) {
         String fechaFinal;
 
-        fechaFinal = fecha.substring(0,2) + "/";
-        fechaFinal += fecha.substring(2,4) + "/";
-        fechaFinal += fecha.substring(4,8);
+        fechaFinal = fecha.substring(0, 2) + "/";
+        fechaFinal += fecha.substring(2, 4) + "/";
+        fechaFinal += fecha.substring(4, 8);
 
         return fechaFinal;
+    }
+
+    public void filter(String text) {
+        alimentos.clear();
+        if(text.isEmpty()){
+            alimentos.addAll(alimentosCopia);
+        } else{
+            text = text.toLowerCase();
+            for(Alimento item: alimentosCopia){
+                if(item.getNombreAlimento().toLowerCase().contains(text)){
+                    alimentos.add(item);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
+    private void detalles(int posicion){
+        Intent intent = new Intent(activity, SugerirRecetaActivity.class);
+        intent.putExtra("Alimento", alimentos.get(posicion));
+        intent.putExtra("ClasePadre", "MiNeveraActivity");
+        activity.getApplicationContext().startActivity(intent);
     }
 
     @Override
@@ -137,13 +131,19 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecycl
     }
 
     @Override
-    public void onBindViewHolder(CustomRecyclerViewAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(CustomRecyclerViewAdapter.ViewHolder holder, final int position) {
         llenarFila(holder, position);
+        holder.fila.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detalles(position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return  cursor.getCount();
+        return alimentos.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -153,6 +153,7 @@ public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecycl
         public TextView tvDiasCaducidad;
         public TextView tvFechaCaducidad;
         public ImageView ivFotoAlimento;
+
         public ViewHolder(View itemView) {
             super(itemView);
             fila = itemView;
--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/MiNeveraActivity.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/MiNeveraActivity.java
@@ -1,15 +1,18 @@
 package net.ddns.smartfridge.smartfridgev2.vista;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.Toast;
 
 import net.ddns.smartfridge.smartfridgev2.R;
 import net.ddns.smartfridge.smartfridgev2.modelo.CustomRecyclerViewAdapter;
@@ -21,6 +24,7 @@ public class MiNeveraActivity extends AppCompatActivity {
     private RecyclerView rvMiNevera;
     private CustomRecyclerViewAdapter recyclerViewAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,23 +50,28 @@ public class MiNeveraActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_search, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
 
-        MenuItem menuItem = findViewById(R.id.menuSearch);
-        SearchView searchView = (SearchView) menuItem.getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.menuSearch).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconified(true);
+        searchView.onActionViewCollapsed();
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+                recyclerViewAdapter.filter(s);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
+                    recyclerViewAdapter.filter(s);
                 return false;
             }
         });
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 }

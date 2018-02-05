5a14d701cf6547c6e0bc4e88a6639d20535f9747
raquel <raquel.menciac@gmail.com>
2018-02-05 22:03:16 +0100

    dialog alimento

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/utiles/Dialogos.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/utiles/Dialogos.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -501,4 +502,31 @@ public class Dialogos {
         //Emisión de la notificación. Le damos el id del alimento
         nm.notify(alimento.getId(), notificacion);
     }
+
+    //Método para crear un Dialog solicitando los alimentos de manera manual al usuario
+    public void dialogAgregarAlimentoManual(AlertDialog.Builder builder, LayoutInflater inflater){
+        //Mensaje del Alert
+        builder.setMessage("Introduzca el alimento que quiere añadir a la lista:");
+        //Título
+        builder.setTitle("Añadir manualmente");
+        //Añadimos el layout que hemos creado
+        builder.setView(inflater.inflate(R.layout.dialognewfood, null));
+        //Añadimos los botones
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Asignamos el valor introducido a la variable
+                //setAlimento =  dialog.toString();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //No hacemos nada
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }

72a81232c2005cdbcf85b3ab6f965741f9d1713a
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-05 18:28:41 +0100

    Bottom Navigation
    Ya esta la pagina clc
    
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/fragmentos/MainClc.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/fragmentos/MainClc.java
@@ -24,7 +24,7 @@ public class MainClc extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_clc, container, false);
+        return inflater.inflate(R.layout.activity_crear_lista_compra, container, false);
     }
 
 }

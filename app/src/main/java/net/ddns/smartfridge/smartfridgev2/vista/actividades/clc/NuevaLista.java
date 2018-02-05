432734fde057f5fe1270934100a36ef37d6840af
raquel <raquel.menciac@gmail.com>
2018-02-05 19:33:52 +0100

    floating action buton creado con menú

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/NuevaLista.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/NuevaLista.java
@@ -1,7 +1,14 @@
 package net.ddns.smartfridge.smartfridgev2.vista.actividades.clc;
 
+import android.animation.Animator;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import net.ddns.smartfridge.smartfridgev2.R;
 
@@ -11,5 +18,15 @@ public class NuevaLista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_lista);
+        //Cogemos la referencia al floating action button
+        FloatingActionsMenu fab = (FloatingActionsMenu) findViewById(R.id.fab);
+        //Le asignamos el listener
+        fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Mostramos el menú con las dos opciones
+                }
+            });
+        }
     }
-}
+

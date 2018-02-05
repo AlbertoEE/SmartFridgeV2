d0706a4290f26845eb26d1d3f13c5f19f2437b82
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-05 18:41:01 +0100

    Bottom Navigation
    Ya esta la pagina clc 100%
    
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/fragmentos/MainClc.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/fragmentos/MainClc.java
@@ -1,6 +1,7 @@
 package net.ddns.smartfridge.smartfridgev2.vista.fragmentos;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import net.ddns.smartfridge.smartfridgev2.R;
+import net.ddns.smartfridge.smartfridgev2.vista.actividades.clc.NuevaLista;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +26,24 @@ public class MainClc extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_crear_lista_compra, container, false);
+        View view = inflater.inflate(R.layout.activity_crear_lista_compra, container, false);
+
+        view.findViewById(R.id.ibVerLista).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //pon codigo aqui si quieres que pase algo
+            }
+        });
+
+        view.findViewById(R.id.ibNuevaLista).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), NuevaLista.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }

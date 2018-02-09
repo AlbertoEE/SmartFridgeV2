42c07f2c1e3c979166f9074deb095647e904e623
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-09 20:58:01 +0100

    Recycler view chulos
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/adaptadores/CustomRecyclerViewAdapterListas.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/adaptadores/CustomRecyclerViewAdapterListas.java
@@ -1,6 +1,14 @@
 package net.ddns.smartfridge.smartfridgev2.modelo.adaptadores;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,30 +22,51 @@ import java.util.ArrayList;
  * Created by Alberto on 09/02/2018.
  */
 
-public class CustomRecyclerViewAdapterListas extends RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder> {
+public class CustomRecyclerViewAdapterListas extends RecyclerView.Adapter<CustomRecyclerViewAdapterListas.ViewHolder2> {
     private ArrayList<ListaCompra> listas;
+    private Paint p = new Paint();
 
-    @Override
-    public CustomRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public CustomRecyclerViewAdapterListas(ArrayList<ListaCompra> listaCompras){
+        this.listas = listaCompras;
+    }
+
+    public void addItem(ListaCompra listaCompra) {
+        listas.add(listaCompra);
+        notifyItemInserted(listas.size());
+    }
+
+    public void removeItem(int position) {
+        listas.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, listas.size());
     }
 
     @Override
-    public void onBindViewHolder(CustomRecyclerViewAdapter.ViewHolder holder, int position) {
+    public CustomRecyclerViewAdapterListas.ViewHolder2 onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fila_listas_creadas, parent,
+                false);
+        CustomRecyclerViewAdapterListas.ViewHolder2 viewHolder = new CustomRecyclerViewAdapterListas.ViewHolder2(view);
+        return viewHolder;
+    }
 
+    @Override
+    public void onBindViewHolder(CustomRecyclerViewAdapterListas.ViewHolder2 holder, int position) {
+        holder.tvFechaLista.setText("hola");
+        holder.tvNumeroProductos.setText("hola");
     }
 
     @Override
     public int getItemCount() {
-        return listas.size();
+        //return listas.size();
+        return 8;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder2 extends RecyclerView.ViewHolder {
         public View fila;//La fila completa, para el listener
         public TextView tvFechaLista;
         public TextView tvNumeroProductos;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder2(View itemView) {
             super(itemView);
             fila = itemView;
             tvFechaLista = itemView.findViewById(R.id.tvFechaListaCompra);
--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/TodasListasActivity.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/TodasListasActivity.java
@@ -1,15 +1,107 @@
 package net.ddns.smartfridge.smartfridgev2.vista.actividades.clc;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import net.ddns.smartfridge.smartfridgev2.R;
+import net.ddns.smartfridge.smartfridgev2.modelo.adaptadores.CustomRecyclerViewAdapterListas;
+import net.ddns.smartfridge.smartfridgev2.modelo.basico.ListaCompra;
+
+import java.util.ArrayList;
 
 public class TodasListasActivity extends AppCompatActivity {
+    private CustomRecyclerViewAdapterListas adapter;
+    private RecyclerView recyclerView;
+    private ArrayList<ListaCompra> listas;
+    private Paint p = new Paint();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todas_listas);
+
+        cargarRecyclerView();
     }
+
+    private void cargarRecyclerView(){
+        listas = new ArrayList<>();
+
+        adapter = new CustomRecyclerViewAdapterListas(listas);
+        recyclerView = (RecyclerView) findViewById(R.id.rvListas);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        initSwipe();
+    }
+
+    private void initSwipe(){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                if (direction == ItemTouchHelper.LEFT){
+                   // adapter.removeItem(position);
+                } else {
+                    //hacia derecha
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                Bitmap icon;
+                if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
+
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if(dX > 0){
+                        p.setColor(Color.parseColor("#388E3C"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_details_white_18dp);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    } else {
+                        p.setColor(Color.parseColor("#D32F2F"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_delete_white_18dp);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+    private void removeView(){
+
+    }
+
 }

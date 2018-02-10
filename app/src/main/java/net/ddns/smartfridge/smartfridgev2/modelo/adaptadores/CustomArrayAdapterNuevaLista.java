e05e20df1aa21d688846f9504818fbda1a90ce41
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-10 20:10:38 +0100

    ADAPTADOR 4
    -Alberto-

---
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/adaptadores/CustomArrayAdapterNuevaLista.java
@@ -0,0 +1,79 @@
+package net.ddns.smartfridge.smartfridgev2.modelo.adaptadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import net.ddns.smartfridge.smartfridgev2.R;
+import net.ddns.smartfridge.smartfridgev2.modelo.basico.ComponenteListaCompra;
+import net.ddns.smartfridge.smartfridgev2.modelo.basico.ListaCompra;
+
+import java.util.ArrayList;
+
+import cn.refactor.library.SmoothCheckBox;
+
+/**
+ * Created by Alberto on 10/02/2018.
+ */
+
+public class CustomArrayAdapterNuevaLista extends ArrayAdapter<ComponenteListaCompra> {
+    private ArrayList<ComponenteListaCompra> productos;
+    private ArrayList<ComponenteListaCompra> auxiliar;
+    private ArrayList<SmoothCheckBox> checkBoxes;
+
+    public CustomArrayAdapterNuevaLista(@NonNull Context context, ArrayList<ComponenteListaCompra> productosSugeridos) {
+        super(context, R.layout.fila_producto_nueva_lista);
+        this.productos = productosSugeridos;
+    }
+
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final String alimento = productos.get(position).getNombreElemento();
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fila_producto_nueva_lista, parent, false);
+        }
+
+        TextView tvAlimentoSugerido = convertView.findViewById(R.id.tvNombreroductoNuevaLista);
+        SmoothCheckBox scb = convertView.findViewById(R.id.smoothCheckBoxNuevaLista);
+        scb.setVisibility(View.INVISIBLE);
+        checkBoxes.add(scb);
+
+        tvAlimentoSugerido.setText(alimento);
+        scb.setOnCheckedChangeListener(new SmoothCheckBox.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SmoothCheckBox smoothCheckBox, boolean b) {
+                if(b){
+                    auxiliar.add(productos.get(position));
+                } else {
+                    auxiliar.remove(alimento);
+                }
+            }
+        });
+
+        return convertView;
+    }
+
+
+    public void addProducto(ComponenteListaCompra producto) {
+        productos.add(producto);
+        this.notifyDataSetChanged();
+    }
+
+    public void confirmarCambios(){
+        this.productos = this.auxiliar;
+    }
+
+    public void mostrarCheckboxes(){
+        for (SmoothCheckBox item: this.checkBoxes) {
+            item.setVisibility(View.VISIBLE);
+        }
+    }
+}

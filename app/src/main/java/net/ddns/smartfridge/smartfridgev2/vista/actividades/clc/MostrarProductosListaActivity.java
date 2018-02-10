8ffe715380823b545f535379e3caf88aa7ca28d7
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-10 18:13:32 +0100

    Recycler view chulos
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/MostrarProductosListaActivity.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/MostrarProductosListaActivity.java
@@ -4,21 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import net.ddns.smartfridge.smartfridgev2.R;
 
+import java.util.List;
+
 public class MostrarProductosListaActivity extends AppCompatActivity {
     private Intent intent;
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar_productos_lista);
 
         intent = getIntent();
+        cargarAdapter();
     }
 
     private void cargarAdapter(){
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, (R.layout.fila_producto),
+                (R.id.tvNombreProductoFilaProducto),
+                intent.getStringArrayExtra("ListaProductos"));
+
+        listView = (ListView) findViewById(R.id.lvMostrarProductos);
+
+        listView.setAdapter(arrayAdapter);
     }
 }

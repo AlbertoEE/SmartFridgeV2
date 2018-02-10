174f4fb5f63c66e2a8e009137eea3a474c44e8b7
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-10 19:19:19 +0100

    Ultimo listView
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/MostrarProductosListaActivity.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/MostrarProductosListaActivity.java
@@ -3,8 +3,11 @@ package net.ddns.smartfridge.smartfridgev2.vista.actividades.clc;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 
 import net.ddns.smartfridge.smartfridgev2.R;
 
@@ -13,6 +16,8 @@ import java.util.List;
 public class MostrarProductosListaActivity extends AppCompatActivity {
     private Intent intent;
     private ListView listView;
+    private ShareActionProvider mShareActionProvider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +36,27 @@ public class MostrarProductosListaActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.lvMostrarProductos);
 
         listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_mostrar_productos, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        // Return true to display menu
+        return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 }

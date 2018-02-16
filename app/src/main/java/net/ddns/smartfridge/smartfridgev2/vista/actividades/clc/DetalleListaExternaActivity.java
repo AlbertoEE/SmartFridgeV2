9d111eb50888e60ac0119c9df40de6c03b800fe7
raquel <raquel.menciac@gmail.com>
2018-02-16 17:57:56 +0100

    ok visualizar datos en carro

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/DetalleListaExternaActivity.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/actividades/clc/DetalleListaExternaActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 
 import net.ddns.smartfridge.smartfridgev2.R;
+import net.ddns.smartfridge.smartfridgev2.modelo.adaptadores.CustomArrayAdapter;
+import net.ddns.smartfridge.smartfridgev2.modelo.adaptadores.CustomArrayAdapterNuevaLista;
 import net.ddns.smartfridge.smartfridgev2.modelo.basico.ComponenteListaCompra;
 
 import java.util.ArrayList;
@@ -13,7 +16,7 @@ import java.util.ArrayList;
 public class DetalleListaExternaActivity extends AppCompatActivity {
     private ArrayList<ComponenteListaCompra> componentes;//Para cargar los componentes seleccionados
     private ArrayList<ComponenteListaCompra> componentesAdapter;//Para cargar los componentes seleccionados en el adapter
-    //private ComponenteListaCompra c;//Para crear los componentes y pasarlos de un adapter a otro
+    private CustomArrayAdapterNuevaLista adapter;;//Adapter para la vista
     private Intent intent;//Para recoger los datos del activity que de origen
 
     @Override
@@ -27,5 +30,19 @@ public class DetalleListaExternaActivity extends AppCompatActivity {
             componentesAdapter.add(componentes.get(i));
             Log.d("componente", "total: " + componentesAdapter.get(i).getNombreElemento());
         }
+        cargarAdapter();
+    }
+
+    private void cargarAdapter(){
+        if(componentesAdapter != null){
+            adapter = new CustomArrayAdapterNuevaLista(this, componentesAdapter, this);
+        } else {
+            adapter = new CustomArrayAdapterNuevaLista(this, new ArrayList<ComponenteListaCompra>(), this);
+        }
+        //Log.d("elputo", "cargarAdapter: " + crearArray());
+        Log.d("elputo", "cargarAdapter: " + adapter);
+        ListView listView = (ListView)findViewById(R.id.detalle_lista);
+        Log.d("elputo", "cargarAdapter: " + listView);
+        listView.setAdapter(adapter);
     }
 }

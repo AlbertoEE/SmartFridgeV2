c180fbb6645753f589572a06a611c0443195637e
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-02-17 16:38:32 +0100

    TODOS ACTUALIZADOS
    -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/CosasQueHacer.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/CosasQueHacer.java
@@ -12,22 +12,13 @@ public class CosasQueHacer {
 
     //TODO organizar los recursos via nombre
 
-    //TODO revisar permisos cámara módulo CA
-
     //TODO PARA TERMINAR LA PARTE DE CREAR LISTAS:
     /*
     1.TODO Hacer toda la parte de lista externa -EN CURSO-
-    //2.TODO Borrar elementos de la lista -ALBERTO- HECHO
-    //3.TODO Modificar elementos de la lista -ALBERTO- HECHO
     7.TODO Abrir una lista cuando se pulse en ella -RAQUEL Y ALBERTO-
-    //9.TODO poner título a la lista de alimentos: "Seleccionelos alimentos que quiere eliminar a la lista de la compra" o algo similar "definicion explicativa" "SUGERIDOS" -ALBERTO- HECHO
-    10.TODO Tener en cuenta que al pulsar una notificacion del tipo escasez mientras se está haciendo una lista, el producto no se añade. -RAQUEL Y ALBERTO-
-    //11.TODO Solucionar NullPointer en NuevaListaActivity línea 251, cuando salen las sugerencias y se da para atrás sin seleccionar nada -ALBERTO CREO QUE ESTA SOLUCIONADO- HECHO
+    10.TODO Tener en cuenta que al pulsar una notificacion del tipo escasez mientras se está haciendo una lista, el producto no se añade. -RAQUEL-
     12.TODO organizar los productos de la lista via nombre -ALBERTO TODAS LAS LISTAS ORGANIZAR-
-    //13.TODO Mirar por qué cuando no se selecciona nada de lo sugerido, se agrega a la lista HECHO
-    //14.TODO Una vez que confirmas la lista nueva hacer un finish HECHO
     15.TODO Optimizar la carga de imágenes cuando se de a CompraExterna IMPOSIBLE
-    16.TODO arreglar error al editar y volver a dar editar en lista nueva
-    17.TODO hacer que no se puedan repetir los alimentos en la lista de la compra
+    17.TODO hacer que no se puedan repetir los alimentos en la lista de la compra -ALBERTO-
      */
 }

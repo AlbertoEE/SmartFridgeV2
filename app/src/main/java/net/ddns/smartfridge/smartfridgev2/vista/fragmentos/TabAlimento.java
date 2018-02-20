397fc177871238544b860ac11b10c1ca93b802db
raquel <raquel.menciac@gmail.com>
2018-02-20 12:52:12 +0100

    preparando view

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/fragmentos/TabAlimento.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/vista/fragmentos/TabAlimento.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
 import net.ddns.smartfridge.smartfridgev2.R;
 
@@ -31,5 +32,21 @@ public class TabAlimento extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab_alimento, container, false);
     }
+    //Método para realizar la consulta a la bbdd a partir de los datos recogidos
+    public void comprobarRB(View view) {
+        //Miramos si está seleccionado el radiobutton
+        boolean checked = ((me.omidh.liquidradiobutton.LiquidRadioButton) view).isChecked();
+        //Hacemos un case con las opciones de cada radiobutton
+        switch(view.getId()) {
+            case R.id.rbTenga:
+                if (checked)
+                    //
+                    break;
+            case R.id.rbNoTenga:
+                if (checked)
+                    //
+                    break;
 
+        }
+    }
 }

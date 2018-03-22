52813092baa325e86b7c542e9220260f2c4dfeac
MrKratos16 <AlbertoLopezDAM@gmail.com>
2018-03-22 01:22:05 +0100

    ARREGLADOS BUGS VARIOS 8 -Alberto-

--- app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/adaptadores/CustomArrayAdapter.java
+++ app/src/main/java/net/ddns/smartfridge/smartfridgev2/modelo/adaptadores/CustomArrayAdapter.java
@@ -25,6 +25,7 @@ import cn.refactor.library.SmoothCheckBox;
 public class CustomArrayAdapter extends ArrayAdapter<ComponenteListaCompra> {
     private ArrayList<ComponenteListaCompra> alimentos;
     private ArrayList<ComponenteListaCompra> alimentosSeleccionados;
+    private ArrayList<Boolean> booleans;
     private ArrayList<SmoothCheckBox> smoothCheckBoxes;
     private boolean cambio = false;
 
@@ -39,6 +40,7 @@ public class CustomArrayAdapter extends ArrayAdapter<ComponenteListaCompra> {
         this.alimentos = alimentos;
         alimentosSeleccionados = new ArrayList<>();
         smoothCheckBoxes = new ArrayList<>();
+        cargarBooleans();
     }
 
     @NonNull
@@ -61,16 +63,23 @@ public class CustomArrayAdapter extends ArrayAdapter<ComponenteListaCompra> {
             @Override
             public void onCheckedChanged(SmoothCheckBox smoothCheckBox, boolean b) {
                 if(b){
-                    alimentosSeleccionados.add(alimentos.get(position));
+                    booleans.set(position, new Boolean(true));
                     Log.d("RETRASO", "onCheckedChanged: TRUE " + position);
                 } else {
-                    alimentosSeleccionados.remove(alimento);
+                    booleans.set(position, new Boolean(false));
                     Log.d("RETRASO", "onCheckedChanged: FALSO " + position);
                 }
             }
         });
 
         return convertView;
+    }
+
+    public void cargarBooleans(){
+        booleans = new ArrayList<>();
+        for(int i = 0; i < alimentos.size(); i++){
+            booleans.add(new Boolean(true));
+        }
     }
 
     /**
@@ -86,7 +95,6 @@ public class CustomArrayAdapter extends ArrayAdapter<ComponenteListaCompra> {
             for (SmoothCheckBox item: smoothCheckBoxes) {
                 item.setChecked(false, true);
             }
-            alimentosSeleccionados.clear();
         }
     }
 
@@ -96,7 +104,8 @@ public class CustomArrayAdapter extends ArrayAdapter<ComponenteListaCompra> {
      * @return el array list que contiene los alimentos seleccionados por el usuario
      */
     public ArrayList<ComponenteListaCompra> getNuevaLista(){
-        return alimentosSeleccionados;
+
+        return alimentos;
     }
 
     /**
